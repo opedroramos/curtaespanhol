34b6bf97a063d97e44d50d0b02f3003ea3d95511
Pedro Ramos <opedroramosbr@gmail.com>
2022-11-07 20:37:22 -0300

    0

---
+++ script.js
@@ -0,0 +1,19 @@
+setTimeout(
+    function tempo() {
+        var elemento = document.querySelector('#botaopai')
+        var elemento11 = document.querySelector('#pagamentos')
+        var elemento13 = document.querySelector('#vagas')
+        var elemento20 = document.querySelector('#objeções')
+        var elemento21 = document.querySelector('#preço')
+        //var elemento18 = document.querySelector('#provapai')
+
+
+        elemento.style.display = "flex";
+        //elemento11.style.display = "flex";
+        elemento13.style.display = "flex";
+        elemento20.style.display = "flex"
+        elemento21.style.display = "flex"
+        //elemento18.style.display = "flex";
+
+
+    }, /*tempo em milisegundos*/ 500)
